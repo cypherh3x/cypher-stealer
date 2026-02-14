a75f222ca5605a6b1d91954940551c23c766f8c5
Cypher <74319971+cypherh3x@users.noreply.github.com>
2026-02-14 04:34:40 +0200

    Added main.py
    Added main.py (Real Version In The Website)

---
+++ main.py
@@ -0,0 +1,18 @@
+# If you wan't to use Project Cypher, sign up on the website.
+
+# Website: https://ospreypr.xyz
+# Discord: https://ospreypr.xyz/discord
+# Contact me: https://ospreypr.xyz/cypher
+
+import os
+
+def main():
+    print("Project Cypher\nOnly avalaible in the website version.")
+    option = input("1. Discord Server\t 2. Website\nChoose an option: ")
+    if option == "1":
+        os.system("start \"\" https://ospreypr.xyz/discord")
+    elif option == "2":
+        os.system("start \"\" https://ospreypr.xyz")
+
+if __name__ == "__main__":
+    main()
